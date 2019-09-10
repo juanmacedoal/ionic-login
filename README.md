b489e9c2937aed5a950e2eb40393271c5384c1c0
MACEDO ALMEIDA Juan Manuel <juan-manuel.macedo@soprasteria.com>
2019-09-10 08:12:35 +0200

    se fini

---
+++ README.md
@@ -0,0 +1,32 @@
+# Ionic Login with Angular Element Dashboard
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.2.0.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+## Code scaffolding
+
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+
+## Build
+
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
+
+## Build the Webcomponent
+
+Run `npm run build` to build the project.
+
+## Inject the script file
+
+In the index.html inject the script tag with the src route to the file and the file of the Zone.js to make compatible Browsers.
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/zone.js/0.9.1/zone.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.2.10/custom-elements-es5-adapter.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.4.4/polyfill.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.2.10/webcomponents-bundle.js"></script>
+
+## Vars needed
+
+The input are (user) that is the name that will show in screen and the image url in the middle of the page.
--- server.js
+++ server.js
@@ -17,23 +17,23 @@ app.use(
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Origin', '*');
 
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
+  // // Request methods you wish to allow
+  // res.setHeader(
+  //   'Access-Control-Allow-Methods',
+  //   'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  // );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
+  // // Request headers you wish to allow
+  // res.setHeader(
+  //   'Access-Control-Allow-Headers',
+  //   'X-Requested-With,content-type'
+  // );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // // Set to true if you need the website to include cookies in the requests sent
+  // // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
@@ -47,7 +47,7 @@ let server = http.createServer(app);
 server.listen(3000, () => {
   let port = server.address().port;
   console.log('------------------------------------------------');
-  console.log(`Application Address:`);
+  console.log(`Application Address: localhost:${port}`);
 });
 
 /* ************** Server ************** */
